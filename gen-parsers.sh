20b95a765b3269edcad4b4fe8313a6d1954251c1
AMG <andresmargar98@gmail.com>
2025-03-16 18:53:47 +0100

    fix: script

--- gen-parsers.sh
+++ gen-parsers.sh
@@ -4,7 +4,7 @@ for dir in */; do
         (cd "$dir" && tree-sitter generate)
 
         mv "$dir/src/parser.c" "$dir/parser.c"
-        rm -rf "$dir/src/tree_sitter"
+        rm -rf "$dir/tree_sitter"
         mv "$dir/src/tree_sitter" "$dir/tree_sitter"
         rm -rf "$dir/src"
     fi
